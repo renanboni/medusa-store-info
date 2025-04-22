e9a837d0b070b4b065ffba798566507c0d3c83c4
renanboni <r.boni1994@gmail.com>
2025-04-22 12:11:04 +0200

    added middlewares

---
+++ src/api/middlewares.ts
@@ -0,0 +1,29 @@
+import { defineMiddlewares, validateAndTransformBody, validateAndTransformQuery  } from "@medusajs/framework/http";
+import { PostAdminCreateStoreInfo } from "./admin/store-info/validator";
+import { createFindParams } from "@medusajs/medusa/api/utils/validators";
+export const GetStoreInfoSchema = createFindParams()
+
+export default defineMiddlewares({
+    routes: [
+        {
+            matcher: "/admin/store-info",
+            method: "POST",
+            middlewares: [
+                validateAndTransformBody(PostAdminCreateStoreInfo),
+            ],
+        },
+        {
+            matcher: "/admin/store-info",
+            method: "GET",
+            middlewares: [
+                validateAndTransformQuery(
+                    GetStoreInfoSchema,
+                    {
+                        defaults: ["id", "name", "value", "key"],
+                        isList: true,
+                    }
+                ),
+            ],
+        },
+    ],
+})
